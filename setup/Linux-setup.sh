5fdda2b05d50bd0dbd9ba5824aa80595080160e9
Parthav46 <parthav46@gmail.com>
2020-10-02 22:33:04 +0530

    tested linux script

--- setup/Linux-setup.sh
+++ setup/Linux-setup.sh
@@ -1,10 +1,9 @@
+#!/bin/bash
 # Setup script for Linux
-## Untested
-## TODO: Test script on Linux machine
 
 if [[ "$PWD" =~ .*/setup ]]; then 
     cd ..
 fi
 python3 -m virtualenv venv
 source venv/bin/activate
-pip3 install -r requirements.txt
+pip install -r requirements.txt
