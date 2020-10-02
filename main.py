f0164292d880fe578cae9d5fd68c80cce17ead22
Ian Findlay <code@findlayian.com>
2020-10-02 11:02:15 +0100

    Re-add refresh section that was deleted in error

--- main.py
+++ main.py
@@ -50,27 +50,35 @@ def get_data(data):
         except IndexError:
             result_score = "Result Summary Not Found"
 
+    team1.config(text=team_1)
+    team2.config(text=team_2)
+    team1_score.config(text=team_1_score)
+    team2_score.config(text=team_2_score)
+    result.config(text=result_score)
+    team1.update()
+    team2.update()
+    team1_score.update()
+    team2_score.update()
+    result.update()
 
 a = Label(text ='Cricket Live Score by SWAPNIL', font ='arial 8')
+a.grid(row=0, columnspan=2, pady=5)
 team1 = Label(text='Team 1', font='arial 20', bg='light goldenrod')
+team1.grid(row=1, column=0)
 team2 = Label(text='Team 2', font='arial 20', bg='light goldenrod')
-team1_score = Label(root, text='hit refresh', font='arial 20', bg='light goldenrod')
-team2_score = Label(text='hit refresh', font='arial 20', bg='light goldenrod')
+team2.grid(row=1, column=1)
 
+team1_score = Label(root, text='hit refresh', font='arial 20', bg='light goldenrod')
+team1_score.grid(row=2, column=0, padx=5)
+team2_score = Label(text='hit refresh', font='arial 20', bg='light goldenrod')
+team2_score.grid(row=2, column=1, padx=5)
 
 result = Label(root, text='hit refresh', font='arial 11', bg='light goldenrod')
+result.grid(row=3, columnspan=2, pady=5)
+
 data = [team1, team2, team1_score, team2_score, result]
 ref = get_data(data)
 refresh = Button(text='Refresh', command=ref, bg='black', fg='white')
-
-
-# pack
-a.grid(row=0, columnspan=2, pady=5)
-team1.grid(row=1, column=0)
-team2.grid(row=1, column=1)
-team1_score.grid(row=2, column=0, padx=5)
-team2_score.grid(row=2, column=1, padx=5)
-result.grid(row=3, columnspan=2, pady=5)
 refresh.grid(row=4, columnspan=2)
 
 web = Label(root, text='Data Collected from Cricbuzz', font='ariel 8')
