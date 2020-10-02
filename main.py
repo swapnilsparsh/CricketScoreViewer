3b3940dc8608b7b8335fabedff022342b774fef5
GangulyShreyan <gangulyofficialshreyan@gmail.com>
2020-10-03 02:44:37 +0530

    Resolved conflicts

--- main.py
+++ main.py
@@ -2,11 +2,32 @@ from tkinter import *
 
 import requests
 from bs4 import BeautifulSoup
+import time
+
+start = time.time()
 
 root = Tk()
 root.configure(bg='sandybrown')
 root.title("Cricket Score Viewer by SWAPNIL")
 root.geometry("350x183")
+onImg = PhotoImage(file="onbutton.png")
+offImg = PhotoImage(file="offbutton.png")
+
+def switch():
+    global btnState
+    if btnState:
+        btn.config(image=offImg, bg="#CECCBE", activebackground="#CECCBE")
+        root.config(bg="#CECCBE")
+        txt.config(text="Dark Mode: OFF", bg="#CECCBE")
+        btnState = False
+    else:
+        btn.config(image=onImg, bg="#2B2B2B", activebackground="#2B2B2B")
+        root.config(bg="#2B2B2B")
+        txt.config(text="Dark Mode: ON", bg="#2B2B2B")
+        btnState = True
+
+
+btnState = False
 
 def get_data(data):
     team1, team2, team1_score, team2_score, result = data
@@ -26,7 +47,7 @@ def get_data(data):
         team_2 = "Name Not Found"
 
     try:
-        team_1_score = soup.find_all(class_='cb-ovr-flo')[10].get_text()
+        team_1_score = soup.find_all(class_='cb-ovr-flo')[8].get_text()
         if team_1_score == "":
             team_1_score = "0"
 
@@ -34,7 +55,7 @@ def get_data(data):
         team_1_score = "Score Not Found"
 
     try:
-        team_2_score = soup.find_all(class_='cb-ovr-flo')[12].get_text()
+        team_2_score = soup.find_all(class_='cb-ovr-flo')[10].get_text()
         if team_2_score == "":
             team_2_score = "0"
     except IndexError:
@@ -64,7 +85,7 @@ def get_data(data):
 a = Label(text ='Cricket Live Score by SWAPNIL', font ='arial 8')
 a.grid(row=0, columnspan=2, pady=5)
 team1 = Label(text='Team 1', font='arial 20', bg='light goldenrod')
-team1.grid(row=1, column=0)
+team1.grid(row=1, column=0,padx=15)
 team2 = Label(text='Team 2', font='arial 20', bg='light goldenrod')
 team2.grid(row=1, column=1)
 
@@ -78,10 +99,34 @@ result.grid(row=3, columnspan=2, pady=5)
 
 data = [team1, team2, team1_score, team2_score, result]
 ref = get_data(data)
-refresh = Button(text='Refresh', command=ref, bg='black', fg='white')
+
+refresh = Label(text='Scores are refreshed every 2 seconds.', font='arial 8', bg='sandybrown')
 refresh.grid(row=4, columnspan=2)
 
 web = Label(root, text='Data Collected from Cricbuzz', font='ariel 8')
 web.grid(row=5, columnspan=2, pady=0)
 
-root.mainloop()
+txt = Label(root, text="Dark Mode: OFF", font="FixedSys 17", bg="#CECCBE", fg="green")
+txt.grid(row=8, columnspan=2)
+
+btn = Button(root, text="OFF", borderwidth=0, command=switch, bg="#CECCBE", activebackground="#CECCBE", pady=1)
+btn.grid(row=7, columnspan=2,pady=20)
+
+btn.config(image=offImg)
+
+stat = True
+def update_stat():
+    global stat
+    stat = False
+
+root.protocol("WM_DELETE_WINDOW", update_stat)
+
+while(stat == True):
+    end = time.time()
+    if(abs(((end-start)%2)-0)<=0.2):
+        data = [team1, team2, team1_score, team2_score, result]
+        ref = get_data(data)
+    root.update_idletasks()
+    root.update()
+    time.sleep(0.01)
+
