4ac8ca3ec4e975c1f3627ba2b32c8ff377c9821d
mangoomeh <67454525+mangoomeh@users.noreply.github.com>
2020-10-06 15:24:04 +0800

    Changed try/except to simple if clause and fixed dark/light theme

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 refresh_time = 1000* 2 # Refresh every 2 seconds
 
 # Background colors
-original_bg = '#CECCBE'
+original_bg = "#CECCBE"
 dark_bg = "#2B2B2B"
 
 # Set up tkinter root window
@@ -14,22 +14,26 @@ root = Tk()
 root.title("Cricket Score Viewer by SWAPNIL")
 root.configure(bg=original_bg)
 
-# Darkmode (button images, function, )
+# Darkmode button images
 onImg = PhotoImage(file="onbutton.png")
 offImg = PhotoImage(file="offbutton.png")
+
 def darkmode_switch():
     # Check current bg colour
     current_bg = root.cget('bg')
+    
     # If current_bg is original, change new_bg to dark (vice versa)
     if current_bg == original_bg:
         new_bg = dark_bg
         darkmodetxt_label.config(text="Dark Mode: ON", bg=new_bg)
+        darkmode_btn.config(image=onImg, bg=new_bg, activebackground=new_bg)
     elif current_bg == dark_bg:
         new_bg = original_bg
         darkmodetxt_label.config(text="Dark Mode: OFF", bg=new_bg)
+        darkmode_btn.config(image=offImg, bg=new_bg, activebackground=new_bg)
     
     # Set bg to new_bg, fg to current_bg
-    darkmode_btn.config(image=offImg, bg=new_bg, activebackground=new_bg)
+    
     root.config(bg=new_bg)
     for item in all_objects:
         item.config(bg=new_bg, fg=current_bg)
@@ -37,40 +41,40 @@ def darkmode_switch():
 def get_data():
     """A helper function which fetch the data and update the UI"""
     
-    # Set data to scrape from headersite
-    var_name_list = ['team1', 'team2', 'team1_score', 'team2_score', 'result']
-    soup_find_list = {
-                    'cb-ovr-flo cb-hmscg-tm-nm' : (0, 1),
-                    'cb-ovr-flo' : (8, 10),
-                    'cb-ovr-flo cb-text-live' : (0,)
-                    }
-
     # URL Request
     url ='https://www.cricbuzz.com/'
     page = requests.get(url)
     soup = BeautifulSoup(page.text,'html.parser')
-    
-    # Store values in dictionary object
-    data = {}  # Use a dictionary to store header crawling data
-    iteration = 0
-    for k, v in soup_find_list.items():
-        for i in v:
-            try:
-                soup_found = soup.find_all(class_=k)[i].get_text()
-                if soup_found == None:
-                    data[var_name_list[iteration]] = '-'
-                else:
-                    data[var_name_list[iteration]] = soup_found
-            except IndexError:
-                data[var_name_list[iteration]] = 'N.A.'
-            iteration += 1
+
+    # Find our data
+    team_names = soup.find_all(class_='cb-ovr-flo cb-hmscg-tm-nm')
+    team_scores = soup.find_all(class_='cb-ovr-flo')
+    result1 = soup.find_all(class_='cb-ovr-flo cb-text-live') # empty if no ongoing game
+    result2 = soup.find_all(class_='cb-ovr-flo cb-text-complete')
+
+    # Get name of first 2 teams competing
+    dteam1 = team_names[0].get_text()
+    dteam2 = team_names[1].get_text()
+
+    # Check if there is an ongoing game & save into variables data we want
+    if not result1: # check if result1 is an empty list
+        dresult = result2[0].get_text()
+        dteam1_score = team_scores[10].get_text()
+        dteam2_score = team_scores[12].get_text()
+
+    else:
+        dresult = result1[0].get_text()
+        dteam1_score = team_scores[8].get_text()
+        dteam2_score = team_scores[10].get_text()
 
     # Update the text labels
-    team1.config(text=data.get('team1'))
-    team2.config(text=data.get('team2'))
-    team1_score.config(text=data.get('team1_score'))
-    team2_score.config(text=data.get('team2_score'))
-    result.config(text=data.get('result'))
+    team1.config(text=dteam1)
+    team2.config(text=dteam2)
+    team1_score.config(text=dteam1_score)
+    team2_score.config(text=dteam2_score)
+    result.config(text=dresult)
+    
+    # Loop itself
     root.after(refresh_time, get_data)
 
 # Initialise Tkinter objects
@@ -80,7 +84,7 @@ team2 = Label(text='Team 2', font='arial 20', bg=original_bg)
 team1_score = Label(root, text='hit refresh', font='arial 20', bg=original_bg)
 team2_score = Label(text='hit refresh', font='arial 20', bg=original_bg)
 result = Label(root, text='hit refresh', font='arial 11', bg=original_bg)
-refresh = Button(text='Refresh', command=get_data, bg=original_bg, fg=dark_bg)
+refresh = Button(text='Refresh', command=get_data, bg=original_bg, fg=dark_bg) # Force refresh
 header = Label(root, text='Data Collected from Cricbuzz', font='ariel 8')
 darkmodetxt_label = Label(root, text="Dark Mode: OFF", font="FixedSys 17", bg=original_bg, fg="green")
 darkmode_btn = Button(root, image=offImg, borderwidth=0, command=darkmode_switch, bg=original_bg, activebackground=original_bg, pady=1)
@@ -92,15 +96,16 @@ team2.grid(             row=1, column=1)
 team2_score.grid(       row=2, column=1, padx=5)
 team1_score.grid(       row=2, column=0, padx=5)
 result.grid(            row=3, columnspan=2, pady=5)
-refresh.grid(           row=4, columnspan=2)
-header.grid(               row=5, columnspan=2, pady=0)
+refresh.grid(           row=4, columnspan=2, pady=5)
+header.grid(            row=5, columnspan=2, pady=0)
 darkmodetxt_label.grid( row=8, columnspan=2)
 darkmode_btn.grid(      row=7, columnspan=2, pady=20)
 
+# Objects for which we want to follow the dark/light theme
 all_objects = [team1, team2, team1_score, team2_score, result, refresh]
 
 # Run get_data after mainloop starts
-root.after(0, get_data)
+root.after(0, get_data) # This triggers get_data which has a root.after ==> hence loops itself
 
 # Run the app
 try:
